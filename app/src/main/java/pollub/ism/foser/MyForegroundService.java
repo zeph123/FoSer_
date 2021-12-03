19d07251536ebb84f4eea236d74df006910a01bc
zeph123 <zygmuntlata39@gmail.com>
2021-12-03 18:11:55 +0100

    Laboratorium 9.

--- app/src/main/java/pollub/ism/foser/MyForegroundService.java
+++ app/src/main/java/pollub/ism/foser/MyForegroundService.java
@@ -5,14 +5,19 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MyForegroundService extends Service {
 
@@ -22,17 +27,65 @@ public class MyForegroundService extends Service {
     public static final String TIME = "time";
     public static final String WORK = "work";
     public static final String WORK_DOUBLE = "work_double";
+
     private String message;
     private Boolean show_time, do_work, double_speed;
+    private final long period = 2000;
+
+    private Context ctx;
+    private Intent notificationIntent;
+    private PendingIntent pendingIntent;
+
+    private int counter;
+    private Timer timer;
+    private TimerTask timerTask;
+
+    final Handler handler = new Handler();
+
+    final Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            Notification notification = new Notification.Builder(ctx, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_my_icon)
+                    .setContentTitle(getString(R.string.ser_title))
+                    .setShowWhen(show_time)
+                    .setContentText(message + " " + String.valueOf(counter))
+                    .setLargeIcon(BitmapFactory.decodeResource (getResources() , R.drawable.circle ))
+                    .setContentIntent(pendingIntent)
+                    .build();
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.notify(1,notification);
+        }
+    };
 
     @Override
     public void onCreate() {
         super.onCreate();
+        ctx = this;
+        notificationIntent = new Intent(ctx, MainActivity.class);
+        pendingIntent = PendingIntent.getActivity(this,0,notificationIntent,0);
+
+        counter = 0;
+
+        timer = new Timer();
+
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                counter++;
+                handler.post(runnable);
+            }
+        };
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        handler.removeCallbacks(runnable);
+        timer.cancel();
+        timer.purge();
+        timer = null;
     }
 
     @Nullable
@@ -52,8 +105,8 @@ public class MyForegroundService extends Service {
 
         createNotificationChannel();
 
-        Intent notificationIntent = new Intent(this,MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,notificationIntent,0);
+//        Intent notificationIntent = new Intent(this,MainActivity.class);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,notificationIntent,0);
 
 
         Notification notification = new Notification.Builder(this,CHANNEL_ID)
@@ -73,20 +126,22 @@ public class MyForegroundService extends Service {
     }
 
     private void doWork() {
-
-        try {
-            Thread.sleep(5000);
-
-        } catch (Exception e) {
-            //
+        if(do_work) {
+            timer.schedule(timerTask, 0L, double_speed ? period / 2L : period);
         }
-
-        String info = "Start working..."
-                +"\n show_time=" + show_time.toString()
-                +"\n do_work=" + do_work.toString()
-                +"\n double_speed=" + double_speed.toString();
-
-        Toast.makeText(this, info ,Toast.LENGTH_LONG).show();
+//        try {
+//            Thread.sleep(5000);
+//
+//        } catch (Exception e) {
+//            //
+//        }
+//
+//        String info = "Start working..."
+//                +"\n show_time=" + show_time.toString()
+//                +"\n do_work=" + do_work.toString()
+//                +"\n double_speed=" + double_speed.toString();
+//
+//        Toast.makeText(this, info ,Toast.LENGTH_LONG).show();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
